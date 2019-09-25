71c9cce2f4a1a052c0d1281f9e3f6c9211e4cb27
DeadMan-MOC <mokgohloallie@gmail.com>
2019-09-25 13:50:44 +0200

    --- Auto-Git Commit ---

--- spec/personSpec.js
+++ spec/personSpec.js
@@ -1,6 +1,5 @@
 describe("", ()=>{
-    
     it ("", ()=> {
-        // expect(person_instance.add(0,0)).toBe(0);
+        expect(person.hello("Hello, my name is Ryan and I am 30 years old. My interests are being a hardarse,agile and ssd hard drives.")).toEqual(`Hello, my name is ${this.name} and I am ${this.age} years old. My interests are ${this.interests} and `+a +'.');
     })
 })
